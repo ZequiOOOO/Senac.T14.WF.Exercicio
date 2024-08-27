4b059421d4df3d9b47a22acf96ffa6e84e9658a1
Ezequi0000 <ezequielramos2299@gmail.com>
2024-08-26 21:00:45 -0300

    Aplicação.MVC

--- PrimeiraAplicação/PrimeiraAplicaçãoWebMVC/Controllers/HomeController.cs
+++ PrimeiraAplicação/PrimeiraAplicaçãoWebMVC/Controllers/HomeController.cs
@@ -6,6 +6,13 @@ namespace PrimeiraAplicaçãoWebMVC.Controllers
 {
     public class HomeController : Controller
     {
+        private static List<Usuario> _list = new List<Usuario>()
+        {
+             new Usuario() { Id = 1, nome = "Dom Juan", email = "DonJuan@gmail.com"},
+             new Usuario() { Id = 2, nome = "Diogo", email = "diogo22@gamial.com"},
+             new Usuario() { Id = 3, nome = "Homer", email = "homerSimp@gmail.com"},
+        };
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -27,19 +34,56 @@ namespace PrimeiraAplicaçãoWebMVC.Controllers
             return View();
         }
 
+        public IActionResult NovoUsuario(int? id)
+        {
+
+            Usuario? usuario = null;
+
+            if (id != null)
+            {
+                usuario = _list.Where(w => w.Id == id).FirstOrDefault();
+            }
+            return View(usuario);
+        }
+
+        public IActionResult PersistirUsuario(int? id, string nome, string email)
+        {
+            if (id == null)
+            {
+                var novoUsuario = new Usuario()
+                {
+                    Id = _list.Count + _list.Last().Id,
+                    nome = nome,
+                    email = email
+                };
+                _list.Add(novoUsuario);
+            }
+            else
+            {
+                var usuario = _list.Where(w => w.Id == id).FirstOrDefault();
+                usuario.nome = nome;
+                usuario.email = email;
+            }
+
+            return RedirectToAction("Usuario");
+        }
+
+   
         public IActionResult Usuario()
         {
-            var Lista = new List<Usuario>()
-            {
-                new Usuario() { Id = 1, nome = "Dom Juan", email = "DonJuan@gmail.com"},
-                new Usuario() { Id = 2, nome = "Diogo", email = "diogo22@gamial.com"},
-                new Usuario() { Id = 3, nome = "Homer", email = "homerSimp@gmail.com"},
-            };
-
-            var ViewModel = new UsuarioViewModel() { ListUsuario = Lista };
+            var ViewModel = new UsuarioViewModel() { ListUsuario = _list };
 
             return View(ViewModel);
 
+        }
+
+        public IActionResult Deletar(int Id)
+        {
+            var usuario = _list.Where(w => w.Id == Id).FirstOrDefault();
+
+            _list.Remove(usuario);
+
+            return RedirectToAction("Usuario"); 
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
