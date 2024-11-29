f348cc27c26411f62cbb488bb78c07ccd721ef5a
Ezequi0000 <ezequielramos2299@gmail.com>
2024-11-28 21:50:06 -0300

    viru.

---
+++ Senac.ConsoleAppinput/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("B");
+Console.Write("o");
+Console.Write("a");
+
+Console.WriteLine("noite");
+
+Console.WriteLine();
+for ( int I = 0; I < 10; I++ )
+{ 
+  for (int i = 0; i < 100; i++)
+  {
+    Console.Write("x");
+    Console.Write("-");
+    Thread.Sleep(10);
+  }
+  Console.WriteLine();
+}
+Console.Write("Ola digite seu nome: ");
+string[] Linhas = Console.ReadLine().Split(" ");
+for (int i = 0; i < Linhas.Length; i++)
+
+{
+    Console.WriteLine(Linhas[i]);
+}
+
+
---
+++ Senca.Console.Viru/Program.cs
@@ -0,0 +1,49 @@
+﻿Console.ForegroundColor = ConsoleColor.Green;
+
+string[] MenssagensInicias =
+{
+    "Inicializado sistema...",
+    "Carregango dados",
+    "Transmitindo arquivos...",
+    "Conectando ao servidor remoto..."
+};
+
+
+foreach (var msg in MenssagensInicias)
+{
+    ImprimirMenssagem(msg, 100);
+    Console.WriteLine();
+    Thread.Sleep(100);
+}
+Console.ForegroundColor = ConsoleColor.Red;
+ImprimirMenssagem("Todos os seus arquivos foram criptografados.", 50);
+Console.WriteLine();
+Thread.Sleep(1000);
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+ImprimirMenssagem("Para recuperar seus dados envie 10 BTC PARA O endereco", 30);
+Console.WriteLine();
+Thread.Sleep(1000);
+
+
+Console.ForegroundColor= ConsoleColor.Green;
+ImprimirMenssagem("\n BTC address: 1AzifFAfdfSSFRE-001", 30);
+Console.WriteLine();
+Thread.Sleep(1000);
+
+
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("Pressione uma tecla para continuar...");
+Console.ReadKey();
+
+
+void ImprimirMenssagem(string texto, int tempo)
+
+{
+    foreach ( char c in texto)
+    {
+        Console.Write(c);
+        Thread.Sleep(tempo);        
+    }
+
+}
